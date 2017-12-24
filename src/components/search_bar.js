0a0d20ba3574813ad1a259d4801b93544f5f5098
ywCN <wangyouhao90@gmail.com>
2017-12-24 12:00:57 -0500

    better import

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+// { Component } is like
+// const Component = React.Component;
 
-const SearchBar = () => {
-    return <input />;
-};
+class SearchBar extends Component {
+    render() {
+        return <input />
+    };
+}
 
 export default SearchBar;
