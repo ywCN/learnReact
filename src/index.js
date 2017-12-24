78c06714a5b9a6f04ff1580dd5dd01370dfff00d
ywCN <wangyouhao90@gmail.com>
2017-12-23 22:59:02 -0500

    display info on web

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 // create a new component. 
 // this component should create some HTML.
 const App = function() {
@@ -6,3 +8,5 @@ const App = function() {
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
+// document.querySelector('.container') is the root container element
