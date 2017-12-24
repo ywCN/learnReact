c703ae205bd45c249264f6b5c0d0581441bdd64d
ywCN <wangyouhao90@gmail.com>
2017-12-24 17:00:34 -0500

    used 'value'

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -24,7 +24,10 @@ class SearchBar extends Component {
     render() {
         return (
             <div>
-                <input onChange={event => this.setState({ term: event.target.value })} />
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} 
+                />
                 Value of the input: {this.state.term}
             </div>
         );
