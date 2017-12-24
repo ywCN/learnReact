be1bccb046d86fcaf69501a2563c0ca42455330a
ywCN <wangyouhao90@gmail.com>
2017-12-24 10:31:46 -0500

    fix dir

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SearchBar from 'components/search-bar';
+import SearchBar from './components/search-bar';
 
 const API = 'AIzaSyAKRu9Q1NFeewHGbsC85jAXa-Cwkm3avZQ';
 // create a new component. 
