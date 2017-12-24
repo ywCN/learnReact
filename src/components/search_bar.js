988be8dbeda4c87d6ec146c7169bb5dd25e892c5
ywCN <wangyouhao90@gmail.com>
2017-12-24 12:11:18 -0500

    set up event handler

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,8 +3,14 @@ import React, { Component } from 'react';
 // const Component = React.Component;
 
 class SearchBar extends Component {
+    // event handler
+    // on + Element Name + Event Name
+    onInputChange() {
+
+    }
+
     render() {
-        return <input />
+        return <input onChange={this.onInputChange} />;
     };
 }
 
