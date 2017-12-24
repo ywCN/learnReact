a86df19d91d4c399551119b439547d24fa2dbc72
ywCN <wangyouhao90@gmail.com>
2017-12-24 10:31:34 -0500

    import search bar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from 'components/search-bar';
+
 const API = 'AIzaSyAKRu9Q1NFeewHGbsC85jAXa-Cwkm3avZQ';
 // create a new component. 
 // this component should create some HTML.
