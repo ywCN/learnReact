7bc99524640102288c131fcb772e4d052af28d6d
ywCN <wangyouhao90@gmail.com>
2017-12-23 23:00:59 -0500

    ES6

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // create a new component. 
 // this component should create some HTML.
-const App = function() {
+const App = () => {
     return <div>mie!</div>;
 }
 
