d696e55fcfcf43f326285c6e97751773ba3143db
ywCN <wangyouhao90@gmail.com>
2017-12-23 22:24:16 -0500

    first component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 // create a new component. 
 // this component should create some HTML.
-
+const App = function() {
+    return <div>mie!</div>;
+}
 
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
