59ba8fdfe021f74c135556d4ba0ed7d8fa2d88ee
ywCN <wangyouhao90@gmail.com>
2017-12-24 19:24:09 -0500

    create video list

---
+++ src/components/video_list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const VideoList = () => {
+    return (
+        // use className to avoid conflict
+        // col-md-4 list-group is from BootStrap
+        <ul className="col-md-4 list-group">
+            
+        </ul>
+    );
+}
+
+export default VideoList;
