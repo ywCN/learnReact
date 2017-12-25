456493abefe945e594f5aa3c53ac476e7459b3a3
ywCN <wangyouhao90@gmail.com>
2017-12-24 22:15:52 -0500

    apply style for search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -23,12 +23,12 @@ class SearchBar extends Component {
 
     render() {
         return (
-            <div>
+            <div className="search-bar">
                 <input
                     value={this.state.term}
                     onChange={event => this.setState({ term: event.target.value })} 
                 />
-                Value of the input: {this.state.term}
+                {/* Value of the input: {this.state.term} */}
             </div>
         );
     };
