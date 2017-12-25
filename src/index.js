cff099bbfce4815da46ae62ea2a21b17cc93272c
ywCN <wangyouhao90@gmail.com>
2017-12-24 20:56:35 -0500

    This does not work

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyAKRu9Q1NFeewHGbsC85jAXa-Cwkm3avZQ';
 
@@ -29,6 +30,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 {/* pass props to VideoList */}
                 <VideoList videos={this.state.videos}/>
                 
