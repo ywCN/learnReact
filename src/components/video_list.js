b0f4dc1b30f8acf8856b7763b6a049814775915a
ywCN <wangyouhao90@gmail.com>
2017-12-24 20:12:48 -0500

    build and use VideoListItem

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 // accept props from App.js
 const VideoList = (props) => {
+    const videoItems = props.videos.map((video) => {
+        return <VideoListItem video={video} />
+    });
     return (
         // use className to avoid conflict
         // col-md-4 list-group is from BootStrap
         <ul className="col-md-4 list-group">
-            {props.videos.length}
+            {/* {props.videos.length} */}
+            {videoItems}
         </ul>
     );
 }
