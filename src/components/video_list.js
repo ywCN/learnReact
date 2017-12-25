e5d32ee42cfef5202d7f72bce5be93d7d98bea70
ywCN <wangyouhao90@gmail.com>
2017-12-24 19:28:16 -0500

    used props

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const VideoList = () => {
+// accept props from App.js
+const VideoList = (props) => {
     return (
         // use className to avoid conflict
         // col-md-4 list-group is from BootStrap
         <ul className="col-md-4 list-group">
-            
+            {props.video.length}
         </ul>
     );
 }
